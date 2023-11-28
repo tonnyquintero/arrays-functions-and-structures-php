7a05655908285fd0f961342db9387af599111661
tonny187 <tonyoanthony@gmail.com>
2023-11-28 17:12:31 -0500

    adding switch

---
+++ switch.php
@@ -0,0 +1,27 @@
+<?php
+
+$person = 2;
+
+switch ($person) {
+    case  1:
+        echo "Su equipo favorito es la juventus";
+        break;
+    case  2:
+        echo "Su equipo favorito es el barcelona";
+        break;
+    case  3:
+        echo "Su equipo favorito es el Real Madrid";
+        break;
+    case  4:
+        echo "Su equipo favorito es el Boca Juniors";
+        break;
+    case  5:
+        echo "Su equipo favorito es la Roma";
+        break;
+    
+    default:
+        echo "Esa persona no está inscrita";
+
+}
+
+echo "\n";
