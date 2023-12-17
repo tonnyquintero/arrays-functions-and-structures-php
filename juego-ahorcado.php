5b012216c7b7c81bdf38633e7d557a52597d34d8
tonny187 <tonyoanthony@gmail.com>
2023-12-17 18:14:44 -0500

    adding a ahorcado game

--- juego-ahorcado.php
+++ juego-ahorcado.php
@@ -1,5 +1,13 @@
 <?php
 
+function clear() {
+    if (PHP_OS === "WINNT") {
+        system("cls");
+    } else {
+        system("clear");
+    }
+};
+
 $possible_words = ["arranque", "torre", "asteroide", "javascript", 
 "scarface", "videojuego", "francia", "zidane", "pelota", "barco"];
 
@@ -15,26 +23,145 @@ $word_length = strlen($chosen_word);
 $discovered_letters = str_pad("", $word_length, "_");
 $attempts = 0;
 
-echo "Palabra de " . $word_length . " letras \n\n";
-echo $discovered_letters . "\n\n";
+// Codigo de Reatax
 
-//Pedimos al usuario que escriba 
-$player_letter = readline("Escribe una letra: ");
-$player_letter = strtolower($player_letter);
+function print_man() {
 
-if (str_contains($chosen_word, $player_letter) ) {
-    //verificamos todas las ocurrencias de esta letra para reemplazarla
-    $offset = 0;
-    while ( ($letter_position = strpos($chosen_word, $player_letter,
-            $offset) ) !== false ) {
-        
-                $discovered_letters[$letter_position] = $player_letter;
-                $offset = $letter_position + 1;
+    global $attempts;
+    
+    switch ($attempts) {
+
+        case 0:
+            echo "
+            +---+
+            |   |
+                |
+                |
+                |
+                |
+            =========
+            ";
+            break;
+           
+        case 1:
+            echo "
+            +---+
+            |   |
+            O   |
+                |
+                |
+                |
+            =========
+            ";
+            break;
+
+        case 2:
+            echo "
+            +---+
+            |   |
+            O   |
+            |   |
+                |
+                |
+            =========
+            ";
+            break;
+
+        case 3:
+            echo "
+            +---+
+            |   |
+            O   |
+           /|   |
+                |
+                |
+            =========
+            ";
+            break;
+
+        case 4:
+            echo "
+            +---+
+            |   |
+            O   |
+           /|\  |
+                |
+                |
+            =========
+            ";
+            break;
+
+        case 5:
+            echo "
+            +---+
+            |   |
+            O   |
+           /|\  |
+           /    |
+                |
+            =========
+            ";
+            break;
+
+        case 6:
+            echo "
+            Me mataste we
+            +---+
+            |   |
+            O   |
+           /|\  |
+           / \  |
+                |
+            =========
+            ";
+            break;
+
     }
-} else {
-    $attempts++;
-    echo "letra incorrecta. Te quedan " . (MAX_ATTEMPS - $attempts) . " intentos";
 
+    echo "\n\n";
 }
+
+// Fin del codigo de Retax
+
+do {
+
+    echo "Palabra de " . $word_length . " letras \n\n";
+    echo $discovered_letters . "\n\n";
+
+    //Pedimos al usuario que escriba 
+    $player_letter = readline("Escribe una letra: ");
+    $player_letter = strtolower($player_letter);
+
+    if (str_contains($chosen_word, $player_letter) ) {
+        //verificamos todas las ocurrencias de esta letra para reemplazarla
+        $offset = 0;
+        while ( ($letter_position = strpos($chosen_word, $player_letter,
+                $offset) ) !== false ) {
+            
+                    $discovered_letters[$letter_position] = $player_letter;
+                    $offset = $letter_position + 1;
+        }
+    } else {
+
+        clear();
+        $attempts++;
+        echo "letra incorrecta. Te quedan " . (MAX_ATTEMPS - $attempts) . " intentos";
+        print_man();
+        sleep(2);
+    }
+    clear();
+
+} while( $attempts < MAX_ATTEMPS && $discovered_letters != $chosen_word );
+
+clear();
+
+if ($attempts < MAX_ATTEMPS) {
+    echo "Felicidades has adivinado la palabra 😁 \n\n";
+} else {
+    echo "Suerte para la proxima amig@ 😐 \n\n";
+};
+
+echo "La palabra es: $chosen_word \n\n";
+echo "Tu descubriste $discovered_letters";
 
 echo "\n";
